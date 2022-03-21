2c36d798c0576aa6498e60a0605b98ed0938f2c3
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2022-03-21 11:36:09 +0100

    ok4

--- listing.php
+++ listing.php
@@ -2,9 +2,8 @@
     require 'requiere/header.php';
     require 'requiere/navbar.php';
     require 'secretxyz123.inc.php';
-    require 'lib_crud_inc.php'
+    require 'lib_crud_inc.php';
 
-    connexionBD();
     $mabd=connexionBD();
     $req = "SELECT * FROM table_lego INNER JOIN table_saga ON table_lego._sg_id = table_saga.sg_id";
     $resultat = $mabd->query($req);
