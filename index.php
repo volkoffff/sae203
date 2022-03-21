3d7c90b07803d44b70154fccbdffd551b1cde979
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2022-03-21 11:27:57 +0100

    ok2

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
         </div>
     </div>
      <div class="btn_div">
-        <a class="btn_see" href="catalogue.php">catalogue</a>
-        <a class="btn_see btn_see2" href="listing.php">rechercher</a>
+        <a class="btn_see" href="listing.php">catalogue</a>
+        <a class="btn_see btn_see2" href="form_recherche.php">rechercher</a>
     </div>
 </div>
 <?php
