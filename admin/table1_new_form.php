3e654403ec5916bc098d861b27b9f546dbadb10b
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2022-03-21 19:25:54 +0100

    ok7

--- admin/table1_new_form.php
+++ admin/table1_new_form.php
@@ -10,17 +10,14 @@
 	    <hr />
 	    <form action="table1_new_valide.php" method="POST" enctype="multipart/form-data">
 	        nom : <input type="text" name="nom" required /><br />
-	        prix (xx.xx) : <input type="number" name="prix" min="0" required /><br />
+	        prix (xx.xx) : <input type="number" name="prix" min="0"  required /><br />
 	        pieces : <input type="number" name="pieces" min="0" required /><br />
 	        genre <select name="genre" required /><br />
-	        <?php
-	            require '../lib_crud.inc.php';
-	            $co=connexionBD();
-	            afficherAuteursOptions2($co);
-	            deconnexionBD($co);
-	        ?>
-	        </select><br />
-	        âge : <input type="number" min="0" name="age" required><br />
+            <option value="mixte">mixte</option>
+            <option value="homme">homme</option>
+            <option value="fille">fille</option>
+            </select>
+	        âge : <input type="number" name="age" min="0" required><br />
 	        Photo : <input type="file" name="nouvelleimage" required> <br/>
 	        logo de la licence lego : <select name="legosaga">        
 	        <?php
