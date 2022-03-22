c2f7764bc9a3f3b53e6ca06794bf94b8782926fc
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2022-03-22 12:13:56 +0100

    ok11

--- lib_crud.inc.php
+++ lib_crud.inc.php
@@ -267,7 +267,8 @@ echo '</table>'."\n";
 // affichage des resultats de recherche
 function afficherResultatRecherche($mabd) {
     $req = 'SELECT * FROM table_lego 
-    INNER JOIN table_saga ON sg_id = table_saga.sg_id WHERE lg_nom like "%'.htmlentities(filter_var($_POST['searchbar'])).'%"';
+    INNER JOIN table_saga ON table_lego._sg_id = table_saga.sg_id 
+    WHERE lg_nom like "%'.htmlentities(filter_var($_POST['searchbar'])).'%"';
     try {
         $resultat = $mabd->query($req);
     } catch (PDOException $e) {
