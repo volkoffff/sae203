fd8cbb9c0faa1f8d600d24d86b8f0ca0109b604f
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2022-03-23 16:37:36 +0100

    bruh

--- admin/table1_update_form.php
+++ admin/table1_update_form.php
@@ -14,23 +14,22 @@
         $id=$_GET['num'];
         $co=connexionBD();
         $boite=getBD($co, $id);
-        var_dump($boite);
         deconnexionBD($co);
     ?>
-	    <form action="table1_update_valide.php" method="POST" enctype="multipart/form-data">
+	    <form class="orgmod" action="table1_update_valide.php" method="POST" enctype="multipart/form-data">
             <input type="hidden" name="num" value="<?php echo $id; ?>" />
-	        nom : <input type="text" name="nom" value="<?php echo $boite['lg_nom']; ?>" required /><br />
-	        prix : <input type="number" name="prix" value="<?php echo $boite['lg_prix']; ?>" min="0"  required /><br />
-	        pieces : <input type="number" name="pieces" value="<?php echo $boite['lg_pieces']; ?>" min="0" required /><br />
-	        genre :<select name="genre"  ?>" required /><br />
+	        <span>nom :</span> <input type="text" name="nom" value="<?php echo $boite['lg_nom']; ?>" required /><br />
+	        <span>prix :</span> <input type="number" name="prix" value="<?php echo $boite['lg_prix']; ?>" min="0"  required /><br />
+	        <span>pieces :</span> <input type="number" name="pieces" value="<?php echo $boite['lg_pieces']; ?>" min="0" required /><br />
+	        <span>genre :</span> <select name="genre"  ?>" required /><br />
             <option value="<?php echo $boite['lg_genre'];?>"> garder le meme genre : <?php echo $boite['lg_genre']; ?> </option>
             <option value="mixte">mixte</option>
             <option value="homme">homme</option>
             <option value="fille">fille</option>
             </select>
-	        âge : <input type="number" name="age" value="<?php echo $boite['lg_age']; ?>" min="0" required><br />
-	        Photo : <input type="file" name="nouvelleimage" required> <br/>
-	        logo de la licence lego : <select name="legosaga">        
+	        <span>âge :</span> <input type="number" name="age" value="<?php echo $boite['lg_age']; ?>" min="0" required><br />
+	        <span>Photo :</span> <input type="file" name="nouvelleimage" required> <br/>
+	        <span>logo de la licence lego :</span> <select name="legosaga">        
 	        <?php
 	            $co=connexionBD();
 	            afficherAuteursOptionsSelectionne($co, $boite['_sg_id']);
