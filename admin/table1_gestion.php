9e6136e996b3d38cecef65b1874e1b12692a8cee
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2022-03-23 08:49:35 +0100

    bruh

--- admin/table1_gestion.php
+++ admin/table1_gestion.php
@@ -9,9 +9,11 @@
     <body>
         <a href="../index.php">Accueil</a>
         <hr />
+        <div class="tktclehaut">
         <h1>Gestion des données</h1>
 
         <p><a href="table1_new_form.php">Ajouter une bande dessinée</a></p>
+        </div>
         <?php
             require '../lib_crud.inc.php';
             $co=connexionBD();
