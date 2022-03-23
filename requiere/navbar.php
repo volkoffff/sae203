18b0db566d8718aba3a3521f61063f492769a621
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2022-03-23 16:26:35 +0100

    bruh

--- requiere/navbar.php
+++ requiere/navbar.php
@@ -12,7 +12,18 @@
                             <div class="search-box">
                                 <button class="btn-search">
                                 <i class="fas fa-search"></i></button>
-                                <input type="text" class="input-search" placeholder="Type to Search...">
+                                <form class="scb" action="reponse_recherche.php" method="POST">
+                                <input type="search" name="nom" id="real" class="input-search" list="boite" placeholder="Type to Search...">
+                                <datalist id="boite">
+                    <?php
+                    // On va afficher ici la datalist
+                    require 'lib_crud.inc.php';
+                    $co=connexionBD();
+                    genererDatalistAuteurs($co);
+                    deconnexionBD($co);
+                    ?>
+                </datalist>
+                                </form>
                             </div>
                         </a></li>
 
