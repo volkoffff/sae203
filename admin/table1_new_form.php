f9f99f45a8052a36094279abab64e401c97de5aa
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2022-03-24 08:39:05 +0100

    bruh3

--- admin/table1_new_form.php
+++ admin/table1_new_form.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 	    <title>SAE203</title>
+		<link rel="stylesheet" href="../style.css">
 	</head>
 	<body style="font-family:sans-serif;">
 	    <a href="../index.php">Accueil</a> | <a href="admin.php">Gestion</a>
@@ -9,25 +10,46 @@
 	    <h1>Ajouter une boite de lego</h1>
 	    <hr />
 	    <form action="table1_new_valide.php" method="POST" enctype="multipart/form-data">
-	        nom : <input type="text" name="nom" required /><br />
-	        prix (xx.xx) : <input type="number" name="prix" min="0"  required /><br />
-	        pieces : <input type="number" name="pieces" min="0" required /><br />
-	        genre <select name="genre" required /><br />
-            <option value="mixte">mixte</option>
-            <option value="homme">homme</option>
-            <option value="fille">fille</option>
-            </select>
-	        âge : <input type="number" name="age" min="0" required><br />
-	        Photo : <input type="file" name="nouvelleimage" required> <br/>
-	        logo de la licence lego : <select name="legosaga">        
-	        <?php
-	            require '../lib_crud.inc.php';
-	            $co=connexionBD();
-	            afficherAuteursOptions($co,);
-	            deconnexionBD($co);
-	        ?>
-	        </select><br />
-	        <input type="submit" value="Ajouter" />
+			<div class="ee">
+	        	<span>nom :</span> 
+				<input type="text" name="nom" required /><br />
+			</div>
+			<div class="ee">
+	        	<span>prix :</span> 
+				<input type="number" name="prix" min="0"  required /><br />
+			</div>
+			<div class="ee">
+				<span>pieces :</span> 
+				<input type="number" name="pieces" min="0" required /><br />
+			</div>
+			<div class="ee">
+				<span>genre :</genre>	
+					<select name="genre" required /><br />
+						<option value="mixte">mixte</option>
+						<option value="homme">homme</option>
+						<option value="fille">fille</option>
+            		</select>
+			</div>
+			<div class="ee">
+				<span>âge :</span> <input type="number" name="age" min="0" required><br />
+			</div>
+			<div class="ee">
+				<span>Photo :</span> <input type="file" name="nouvelleimage" required> <br/>
+			</div>
+			<div class="ee">
+				<span>logo de la licence lego :</span> 
+					<select name="legosaga">        
+						<?php
+	           			require '../lib_crud.inc.php';
+	            		$co=connexionBD();
+	            		afficherAuteursOptions($co,);
+	            		deconnexionBD($co);
+					?>
+	        		</select>
+					<br />
+	        	<input type="submit" value="Ajouter" />
+			</div>
 	    </form>
+			
 	</body>
 </html>
